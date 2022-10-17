5fde5e35b74c1243259af68f4962587a90127b8c
Paulo Eduardo <pauloef.lopes@gmail.com>
2022-10-17 11:53:06 -0300

    atualizacoes

---
+++ servicos/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent,
+    CriarCursoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
